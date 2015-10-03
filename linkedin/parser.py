a5239f99f55b4132fd17cae3079cf16c1c3d00e0
ishawn <ishawnshan@gmail.com>
2015-10-03 18:33:44 -0400

    finish parser

--- linkedin/parser.py
+++ linkedin/parser.py
@@ -2,9 +2,32 @@
 from bs4 import BeautifulSoup
 import re
 
-class Tool:
-	removeLeft = re.compile()
+class Filter:
+	removeScript = re.compile(r'\\u003.*?\\u003e')
+	removeNextLine = re.compile(r'\\n')
+	removeFmtHeading = re.compile(',"fmt_heading":".*?"')
+	removeFmtBody = re.compile(',"fmt_body":".*?"')
+	removeBlock = re.compile('\},{')
+	removeBlock2 = re.compile('\[')
+	removeBlock3 = re.compile('\]')
+	findContent = re.compile('"fieldName":"(.*?)",.*?:.*?"(.*?)eeking(.*?)"')
 
+	def replace(self, x):
+		x = re.sub(self.removeScript,"",x)
+		x = re.sub(self.removeNextLine," ",x)
+		x = re.sub(self.removeBlock,"\n",x)
+		x = re.sub(self.removeBlock2,"",x)
+		x = re.sub(self.removeBlock3,"",x)
+		x = re.sub(self.removeFmtHeading, "",x)
+		x = re.sub(self.removeFmtBody, "",x)
+		items = re.findall(self.findContent, x)
+		str = ""
+		for item in items:
+			if item[0] == 'Current':
+				str = item[1] + 'eeking' + item[2]
+			if item[0] == 'Summary':
+				str = item[1] + 'eeking' + item[2]
+		return str
 
 
 f = open('test.html')
@@ -14,9 +37,12 @@ pattern = re.compile('snippets":\[\{(.*?)\}\]', re.S)
 items = re.findall(pattern, data)
 print (len(items))
 ##print (data)
-file = open('WriteSnippets.txt', 'w')
+file = open('WriteSnippets1.txt', 'w')
+filter = Filter()
 for item in items:
-	file.write(item + '\n' + '\n')
+	item = filter.replace(item)
+	if len(item) > 0:
+		file.write(item + '\n' + '\n')
 file.close()
 
 
